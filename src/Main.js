a5eb47d680991879ea5f06f058294fa9cd74f7ab
xiaozhenglang <xiaozheng.lang1@gmail.com>
2018-03-31 22:10:09 -0600

    add main

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                this is main
+            </div>
+        );
+    }
+}
